6c322ac622bd5232bd01b1f526b004306519f115
HeisenDelta <ariya8.narayan7@outlook.jp>
2024-08-21 14:11:30 +0900

    Changed number influenced to exclude selected region

--- graphs.py
+++ graphs.py
@@ -3,6 +3,7 @@ import io
 import base64
 import pandas as pd
 import seaborn as sns
+import pickle
 
 import japanize_matplotlib
 from matplotlib.ticker import MaxNLocator
@@ -10,6 +11,8 @@ import matplotlib
 matplotlib.use('Agg')
 
 time_grouped = pd.read_csv('data/time_grouped.csv').set_index('市区町丁')
+with open('data/predict_dicts.pkl', 'rb') as f:
+    predict_dicts = pickle.load(f)
 
 def create_img(fig):
     img = io.BytesIO()
@@ -26,7 +29,7 @@ def case_count_bar_chart(region):
     ax.patch.set_alpha(0)
 
     ax.bar(time_grouped.columns, time_grouped.loc[region].astype(int))
-    ax.set_xlabel('時間', fontsize=12)
+    ax.set_xlabel('時間 (24時制)', fontsize=12)
     ax.set_ylabel('ケース数', fontsize=12)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     
@@ -47,3 +50,13 @@ def case_density_function(region):
 
     
     return create_img(fig)
+
+def top_n_influenced(region, date, n=3):
+    neighbor_keys = list(predict_dicts[region].keys())
+    df = pd.DataFrame([predict_dicts[region][key] for key in neighbor_keys], index=neighbor_keys, columns=pd.date_range('2022-01-01', periods=365, freq='D').strftime('%Y-%m-%d'))
+    df = df.apply(lambda x: (x - x.min()) / (x.max() - x.min()), axis=1)
+    df = df.apply(lambda x: x ** 10)
+    df = df.apply(lambda x: (x - x.min()) / (x.max() - x.min()), axis=1)
+
+    df = df.fillna(0)
+    return df[f'2022-{date}'].drop(region, axis=0).nlargest(n).index.to_list()
